2b4b13e63c2729b48849ac08e305652f40acb896
Jonathan Riecke <jonathanriecke@gmail.com>
2020-01-09 02:12:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,25 +15,25 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  return [].pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  return [].shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  return [index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  return [0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  return [-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  return[index_number] = element
 end
